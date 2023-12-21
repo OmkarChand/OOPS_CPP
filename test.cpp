56083602efcd78dd97af0aaeb44ba1a629a1cf76
OMKAR CHAND DESHLAHRE <111699930+OmkarChand@users.noreply.github.com>
2023-12-21 05:53:47 +0000

    first commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Hero{
+    //propertise
+    int health;
+};
+
+int main()
+{
+    Hero h1;
+ 
+    cout << "size:" << sizeof(h1) << endl;
+
+    return 0;
+}
